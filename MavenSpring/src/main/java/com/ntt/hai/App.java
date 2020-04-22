3b40a8f18f61c5fa01ba1ba2382cabdae9f1d814
Spoorthi Ambarish <spoorthi162@gmail.com>
2020-04-22 19:09:52 +0530

    second unzip commit

---
+++ MavenSpring/src/main/java/com/ntt/hai/App.java
@@ -0,0 +1,12 @@
+package com.ntt.hai;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "my first maven project" );
+        product p=new product();
+        p.printdata();
+    }
+}
---
+++ MavenSpring/src/main/java/com/ntt/hai/product.java
@@ -0,0 +1,23 @@
+package com.ntt.hai;
+
+public class product {
+private int productid;
+private String productname;
+public int getProductid() {
+	return productid;
+}
+public void setProductid(int productid) {
+	this.productid = productid;
+}
+public String getProductname() {
+	return productname;
+}
+public void setProductname(String productname) {
+	this.productname = productname;
+}
+public void printdata()
+{
+System.out.println("data is printed");	
+}
+
+}
